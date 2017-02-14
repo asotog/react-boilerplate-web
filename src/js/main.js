72fe893a0e6ab7fcfac94c8dfc091877cf522103
Alejandro Soto <alejandro.soto@rivetlogic.com>
2017-02-14 14:36:04 -0600

    first commit

---
+++ src/js/main.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import {render} from 'react-dom';
+import mainCss from '../css/main.scss';
+
+render(<h1>Hello, world!</h1>, document.querySelector('contact-component'));
+
+console.log('main.js working...');
+
---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+var path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+const mainStylesExtract = new ExtractTextPlugin('main.css');
+const EXCLUDES = [/node_modules/];
+
+module.exports = {
+    context: path.resolve('src'),
+    entry: {
+        main: './js/main'
+    },
+    output: {
+        path: path.resolve('build/'),
+        publicPath: '/build',
+        filename: '[name].js'
+    },
+    devServer: {
+        contentBase: 'src'
+    },
+    plugins: [
+        mainStylesExtract
+    ],
+    module: {
+        loaders: [{ // Javascript
+            test: /\.js$/,
+            exclude: EXCLUDES,
+            loader: 'babel-loader'
+        }, { // Sass 
+            test: /main\.scss$/,
+            exclude: EXCLUDES,
+            loader: mainStylesExtract.extract({
+                fallback: 'style-loader',
+                use: 'css-loader!autoprefixer-loader!sass-loader'
+            })
+        }]
+    }
+};
